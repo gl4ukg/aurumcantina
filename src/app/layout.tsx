aeca76aa1f7c084edc43eb3a5c220728573e208c
Glauk Thaqi <glaukthaqi15@gmail.com>
2025-09-26 10:24:22 +0200

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,41 +1,45 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import LayoutProvider from "./LayoutProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
-	title: "Aurum Cantina",
-	description: "Aurum Cantina është një lounge bar modern që ofron një kombinim unik të kuzhinës amerikane, italiane dhe meksikane. Me një atmosferë të ngrohtë dhe relaksuese, ne ju presim me ushqim të shijshëm, kafe cilësore dhe një përzgjedhje të gjerë pijesh alkoolike. Vendi ideal për të shijuar momente me miqtë apo për të kaluar një mbrëmje të këndshme.",
-	keywords:
-		"restaurant, food, menu, reservation, booking, restaurant template, restaurant website, restaurant website template, restaurant website design, restaurant website development, restaurant website builder, restaurant website creator, restaurant website generator, restaurant website maker, restaurant website editor, restaurant website designer, restaurant website developer, restaurant website builder, restaurant website creator, restaurant website generator, restaurant website maker, restaurant website editor, restaurant website designer, restaurant website developer",
-	authors: [{ name: "Codesphere LLC", url: "www.codespherellc.com" }],
-	creator: "Codesphere LLC",
-	publisher: "Codesphere LLC",
-	openGraph: {
-		title: "Aurum Cantina",
-		description: "Aurum Cantina është një lounge bar modern që ofron një kombinim unik të kuzhinës amerikane, italiane dhe meksikane. Me një atmosferë të ngrohtë dhe relaksuese, ne ju presim me ushqim të shijshëm, kafe cilësore dhe një përzgjedhje të gjerë pijesh alkoolike. Vendi ideal për të shijuar momente me miqtë apo për të kaluar një mbrëmje të këndshme.",
-	},
-	icons: {
-		icon: "/favicon.jpg",
-	},
-	themeColor: "#000000",
-	viewport: {
-		width: "device-width",
-		initialScale: 1,
-		maximumScale: 1,
-		userScalable: false,
-	},
+  title: "Aurum Cantina",
+  description:
+    "Aurum Cantina është një lounge bar modern që ofron një kombinim unik të kuzhinës amerikane, italiane dhe meksikane. Me një atmosferë të ngrohtë dhe relaksuese, ne ju presim me ushqim të shijshëm, kafe cilësore dhe një përzgjedhje të gjerë pijesh alkoolike. Vendi ideal për të shijuar momente me miqtë apo për të kaluar një mbrëmje të këndshme.",
+  keywords:
+    "restaurant, food, menu, reservation, booking, restaurant template, restaurant website, restaurant website template, restaurant website design, restaurant website development, restaurant website builder, restaurant website creator, restaurant website generator, restaurant website maker, restaurant website editor, restaurant website designer, restaurant website developer, restaurant website builder, restaurant website creator, restaurant website generator, restaurant website maker, restaurant website editor, restaurant website designer, restaurant website developer",
+  authors: [{ name: "Codesphere LLC", url: "www.codespherellc.com" }],
+  creator: "Codesphere LLC",
+  publisher: "Codesphere LLC",
+  openGraph: {
+    title: "Aurum Cantina",
+    description:
+      "Aurum Cantina është një lounge bar modern që ofron një kombinim unik të kuzhinës amerikane, italiane dhe meksikane. Me një atmosferë të ngrohtë dhe relaksuese, ne ju presim me ushqim të shijshëm, kafe cilësore dhe një përzgjedhje të gjerë pijesh alkoolike. Vendi ideal për të shijuar momente me miqtë apo për të kaluar një mbrëmje të këndshme.",
+  },
+  icons: {
+    icon: "/favicon.jpg",
+  },
+  themeColor: "#000000",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body>
-				<LayoutProvider>{children}</LayoutProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>
+        <LayoutProvider>{children}</LayoutProvider>
+        <Analytics />
+      </body>
+    </html>
+  );
 }
